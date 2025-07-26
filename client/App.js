4334026c5035c5f6a61a3f2ef293c551b120f7fe
Khairil Mukmin <khairilmukmin1707@gmail.com>
2025-07-26 15:01:23 +0800

    Create App.js

---
+++ client/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Welcome to LAKUNA FOOD 🍛</h1>
+      <p>Your custom food ordering platform is under construction 🚧</p>
+    </div>
+  );
+}
+
+export default App;
